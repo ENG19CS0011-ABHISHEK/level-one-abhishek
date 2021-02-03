502a79e06401191d0dcb8ef0fc72e963e8e1298d
ENG19CS0011-ABHISHEK <76118828+ENG19CS0011-ABHISHEK@users.noreply.github.com>
2021-02-03 22:33:26 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -1,1 +1,39 @@
 //WAP to find the distance between two point using 4 functions.
+#include<stdio.h>
+#include<math.h>
+
+float inp()
+{
+	float x;
+	scanf("%f",&x);
+	return x;
+}
+
+float squ(float x1, float x2, float y1, float y2)
+{
+	float s;
+	s=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+	return s;
+}
+
+void out(float x1,float x2,float y1,float y2,float sq)
+{
+	printf("The Distance Of %f ,%f ,%f ,%f Is %.3f .",x1,x2,y1,y2,sq);
+}
+
+
+int main()
+{
+	float x1,x2,y1,y2,square_root;
+	printf("Enter the value for  x1 :\n");
+	x1=inp();
+  printf("Enter the value for  x2 :\n");
+	x2=inp();
+  printf("Enter the value for y1 :\n");
+  y1=inp();
+  printf("Enter the value for y2 :\n");
+  y2=inp();
+	square_root=squ(x1,x2,y1,y2);
+	out(x1,x2,y1,y2,square_root);
+	return 0;
+}
