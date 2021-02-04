101142a7141bdee05cd041d78b551d2e2382cbca
ENG19CS0011-ABHISHEK <76118828+ENG19CS0011-ABHISHEK@users.noreply.github.com>
2021-02-04 14:32:34 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -2,38 +2,39 @@
 #include<stdio.h>
 #include<math.h>
 
-float inp()
+void inp(float *x1,float *x2,float *y1,float *y2)
 {
-	float x;
-	scanf("%f",&x);
-	return x;
+	printf("Enter the value for x1 :\n");
+	scanf("%f",&*x1);
+    	printf("Enter the value for x2 :\n");
+	scanf("%f",&*x2);
+    	printf("Enter the value for y1 :\n");
+	scanf("%f",&*y1);
+	printf("Enter the value for y2 :\n");
+	scanf("%f",&*y2);
+
 }
 
-float squ(float x1, float x2, float y1, float y2)
+float dist(float x1, float x2, float y1, float y2)
 {
 	float s;
 	s=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 	return s;
 }
 
+
 void out(float x1,float x2,float y1,float y2,float sq)
 {
-	printf("The Distance Of %f ,%f ,%f ,%f Is %.3f .",x1,x2,y1,y2,sq);
+	printf("The Distance Of %.3f ,%.3f ,%.3f ,%.3f Is %.3f .",x1,x2,y1,y2,sq);
 }
+
 
 
 int main()
 {
 	float x1,x2,y1,y2,square_root;
-	printf("Enter the value for  x1 :\n");
-	x1=inp();
-  	printf("Enter the value for  x2 :\n");
-	x2=inp();
-  	printf("Enter the value for y1 :\n");
-  	y1=inp();
-  	printf("Enter the value for y2 :\n");
-  	y2=inp();
-	square_root=squ(x1,x2,y1,y2);
+    	inp(&x1,&x2,&y1,&y2);
+	square_root=dist(x1,x2,y1,y2);
 	out(x1,x2,y1,y2,square_root);
-	return 0; 
+	return 0;
 }
