6c79811f6763deb1f0df5fdc7c7f07df0d2d044b
ENG19CS0011-ABHISHEK <76118828+ENG19CS0011-ABHISHEK@users.noreply.github.com>
2021-02-12 12:06:48 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,25 +1,32 @@
 //Write a program to find the sum of n different numbers using 4 functions
 #include<stdio.h>
 
+float inp()
+{
+    int n;
+    float sum=0.0,val;
+    printf("Enter the no of elements you want to add : \n");
+    scanf("%d",&n);
+
+    for(int i =0;i<n;i++)
+    {   
+        printf("Element %d : ",(i+1));
+        scanf("%f",&val);
+        sum += val;
+        
+    }
+    return sum;
+}
+
+void out(float total)
+{
+    printf("The sum of Numbers entered is %.3f",total);
+}
+
 int main()
 {
-	float arr[100];
-	int n;
-	float sum=0;
-	printf("Enter the no of elements you want to calculate the sum  : \n");
-	scanf("%d",&n);
-	printf("Enter the elements one-by-one: \n");
-	for(int i=0;i<n;i++)
-	{
-		printf("Element - %d : ",i+1);
-		scanf("%f",&arr[i]);
-	}
-	
-	for(int i=0;i<n;i++)
-	{
-		sum += arr[i];
-	}
-	
-	printf("The sum of the elements is %.3f.",sum);
-	return 0;
+    float s;
+    s= inp();
+    out(s);
+    return 0;
 }
