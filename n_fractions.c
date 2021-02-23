853a336c035807d7578900aeec36542965e76519
ENG19CS0011-ABHISHEK <76118828+ENG19CS0011-ABHISHEK@users.noreply.github.com>
2021-02-23 10:29:38 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,73 +1,89 @@
 //WAP to find the sum of n fractions.
-#include<stdio.h>
+#include <stdio.h>
+#include<math.h>
 
-struct fraction
+struct fractions
 {
-	int num,den;
+    int num,den;
 };
-
-typedef struct fraction fract;
+ 
+typedef struct fractions fract;
 
 fract inp()
 {
-	fract f;
-	printf("Enter the value for numerator : \n");
-	scanf("%d",&f.num);
-	printf("Enter the value for denominator : \n");
-	scanf("%d",&f.den);
-	return f;
+    fract f;
+    printf("Enter the numerator : \n");
+    scanf("%d",&f.num);
+    printf("Enter the denominator : \n");
+    scanf("%d",&f.den);
+    return f;
+     
+}
+
+int gcd(int a,int b)
+{
+    while(a!=b)
+    {
+        if(a>b)
+        {
+            a-=b;
+        }
+       	else
+        {
+            b-=a;
+        }
+    }
+    return a;
 }
 
 fract calc(fract f1,fract f2)
 {
 	fract sum;
-	if(f1.den == f2.den)
-  	{
-		sum.num=f1.num+f2.num;
-		sum.den=f1.den;
-		return sum;
+	sum.num =(f1.num*f2.den)+(f2.num*f1.den);
+	sum.den = f1.den*f2.den;
+	int g=gcd(sum.num,sum.den);
+	sum.num = sum.num/g;
+	sum.den = sum.den/g;
+	return sum;
+	
 }
-	else
-	{
-		sum.num =(f1.num*f2.den)+(f2.num*f1.den);
-		sum.den = f1.den*f2.den;
-		return sum;
-   	 }
+
+fract ainp(fract arr[])
+{
+    int n;
+    printf("Enter the no. of fractions to be calculated : \n");
+    scanf("%d",&n);
+    fract res;
+    arr[n];
+    res.num=0;
+    res.den=1;
+    printf("Enter the fractions One-by-one :\n");
+    for(int i =0;i<n;i++)
+    {
+	printf("Enter the value of Fraction %d\n" ,(i+1));
+	arr[i]=inp();
+    }
+    for(int i = 0;i<n;i++)
+    {
+	res=calc(res,arr[i]);
+    }
+    return res;
+
+}
+
+
+
+void oup(fract ar)
+{	
+	printf("The sum of the fractions is %d/%d .\n",ar.num,ar.den);
 }
 
 int main()
-{
-	int n;
-	printf("Enter the no. of fractions to be calculated : \n");
-	scanf("%d",&n);
-fract res,arr[n];
-res.num=0;
-res.den=1;
-	printf("Enter the fractions One-by-one :\n");
-	for(int i =0;i<n;i++)
-	{
-		printf("Enter the value of Fraction %d\n" ,(i+1));
-		arr[i]=inp();
-	}
-	for(int i = 0;i<n;i++)
-	{
-		res=calc(res,arr[i]);
-}
-int n1=res.num,n2=res.den;
-while(n1!=n2)
-{
-    if(n1>n2)
-    {
-        n1-=n2;
-    }
-    else
-    {
-        n2-=n1;
-    }
-        
+{   
+    fract arr[100],ar;
+    ar=ainp(arr);
+    oup(ar);
+    return 0;
 }
 
-printf("The sum of all the fractions entered is %d/%d .\n",res.num/n1,res.den/n1);
-return 0;
-}
 
